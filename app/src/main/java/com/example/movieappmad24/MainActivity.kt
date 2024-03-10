327bc98f7e65bef315c6afa556573e638e98da7e
David <david.xing@stud.fh-campuswien.ac.at>
2024-03-10 15:37:09 +0100

    Release version: 0.2
    -added ImageComponent, TitleRow, MovieDetails, ExpandableMovieCard
    -Refactoring into smaller, reusable functions

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.movieappmad24
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -22,11 +35,23 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.size.Scale
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,9 +72,9 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior){
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior) {
     CenterAlignedTopAppBar(
-        title = { Text(text = "Movie App") },
+        title = { Text(text = stringResource(id = R.string.app_name)) },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary,
@@ -59,8 +84,8 @@ fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior){
 }
 
 @Composable
-fun BottomAppBar(){
-    NavigationBar{
+fun BottomAppBar() {
+    NavigationBar {
         NavigationBarItem(
             icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
             label = { Text("Home") },
@@ -76,26 +101,91 @@ fun BottomAppBar(){
     }
 }
 
+@Composable
+fun ImageComponent(imageUrl: String, contentDesc: String) {
+    Image(
+        painter = rememberImagePainter(
+            data = imageUrl,
+            builder = {
+                scale(Scale.FILL)
+            }
+        ),
+        contentDescription = contentDesc,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+@Composable
+fun TitleRow(title: String, isExpanded: Boolean, onExpandToggle: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = title)
+        IconButton(onClick = onExpandToggle) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
+                contentDescription = if (isExpanded) "Collapse" else "Expand"
+            )
+        }
+    }
+}
+
+@Composable
+fun MovieDetails(movie: Movie, isExpanded: Boolean) {
+    Column {
+        if (isExpanded) {
+            Text("Director: ${movie.director}")
+            Text("Released: ${movie.year}")
+            Text("Genre: ${movie.genre}")
+            Text("Actors: ${movie.actors}")
+            Text("Rating: ${movie.rating}")
+            HorizontalDivider()
+            Text("Plot: ${movie.plot}")
+        }
+    }
+}
+
+@Composable
+fun ExpandableMovieCard(movie: Movie) {
+    var isExpanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column {
+            ImageComponent(imageUrl = movie.images.first(), contentDesc = movie.title)
+            TitleRow(movie.title, isExpanded) { isExpanded = !isExpanded }
+            MovieDetails(movie, isExpanded)
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MovieList(){
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+fun MovieList(movies: List<Movie> = getMovies()) {
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { TopAppBar(scrollBehavior = scrollBehavior) },
-        bottomBar = { BottomAppBar()},
-    ) {values ->
+        bottomBar = { BottomAppBar() },
+    ) { values ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(values)
         ) {
-            items(100){
-                Text(
-                    text = "Item$it",
-                    modifier = Modifier.padding(16.dp)
-                )
+            items(movies) { movie ->
+                ExpandableMovieCard(movie)
             }
         }
     }
